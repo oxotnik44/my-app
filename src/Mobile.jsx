85fb72e462cdef65c9ae00b09706eed00fa52774
oxotnik44 <artemnovikov1969@gmail.com>
2022-04-06 16:49:26 +0700

    фиксация

---
+++ src/Mobile.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import cat from "src\Otimax Dev logo 3.png";
+const Mobile = () =>{
+    return(
+         <div>
+             <img src={cat} />
+          </div>
+    )
+}
+export default Mobile;
